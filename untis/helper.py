00927479649425a0860ec87483d6a0e6ad1ae20b
pandaa01 <302643455@qq.com>
2017-11-29 10:43:50 +0800

    添加untis文件

---
+++ untis/helper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import os
+def readlist():
+    list1=[
+        ['','',u'请您输入手机/邮箱/用户名'],
+        ['18291875606','',u'请您输入密码'],
+        ['18291875606','123456',u'请您输入验证码']
+    ]
+    return list1
+
+
+# def readTxt(index):
+#     f=open(os.path.join(os.path.dirname(__file__),'log.txt'),'r')
+#     d=f.readlines()
+#     return d[index]
+#
+# print readTxt(2)
+
+
+
+
